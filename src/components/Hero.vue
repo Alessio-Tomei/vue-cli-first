7f4d7dcd7c5c29106af4550bebc16e11736136fe
Alessio-Tomei <alessiotomei22@gmail.com>
2022-01-26 15:44:03 +0100

    Consegna Finita

---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="jumbotron">
+            <Title/>
+            <h1>Vorrei il titolo qui</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import Title from './Hero.vue';
+
+export default {
+  name: 'Hero',
+  components: {
+      Title,
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .jumbotron {
+        width: 100%;
+        height: 400px;
+        background-image: url(https://picsum.photos/1920/400);
+        background-size:cover;
+        background-repeat: no-repeat;
+    }
+</style>
